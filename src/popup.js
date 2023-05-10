4c885a5dce3d6d5064986a639bb3921d3654f803
Isabelle Nguyen <isabelln@usc.edu>
2023-05-09 17:55:19 -0700

    adjust layout

--- src/popup.js
+++ src/popup.js
@@ -1,21 +1,18 @@
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  var activeTab = tabs[0];
+  const activeTab = tabs[0];
   const urlParams = new URLSearchParams(activeTab.url);
   entries = urlParams.entries();
 
-  var i = 0;
+  let i = 0;
   for (const entry of entries) {
     if (i++ == 0) {
       if (entry[0].includes('?')) {
         addUpdateButton();
         addField(entry[0].split('?').pop(), entry[1]);
-      } else {
-        addNotFoundMessage();
-        document.body.style.width = 'auto';
-      }
-
+      } else addNotFoundMessage();
     } else addField(entry[0], entry[1]);
+    if (i == 1) break;
   }
 });
 
@@ -29,8 +26,11 @@ function addNotFoundMessage(){
 }
 
 document.getElementById("add").addEventListener('click', () => {
-  if (document.getElementById('not_found') !== null) document.getElementById('not_found').remove();
+  if (document.getElementById('not_found') !== null) {
+    document.getElementById('not_found').remove();
+  }
 
+  addField("", "");
 });
 
 function addUpdateButton(){
@@ -45,25 +45,20 @@ function addUpdateButton(){
   button.appendChild(document.createTextNode("Update"));
 
   toolbar.appendChild(button);
-  div.appendChild(document.createElement("br"));
 
   document.getElementById("update").addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var activeTab = tabs[0];
+      const activeTab = tabs[0];
       const urlParams = new URLSearchParams(activeTab.url);
-      var entries = urlParams.entries();
-      var newUrl = "";
+      const entries = urlParams.entries();
+      let newUrl = "";
 
-      var i = 0;
-      for (const entry of entries) {
-        if (i++ == 0) {
-          newUrl += entry[0].split('?')[0] + '?';
-        } else break;
-      }
+      const [first] = entries;
+      newUrl += first[0].split('?')[0] + '?';
 
-      var elements = document.querySelectorAll('input[type="text"]');
-
-      for (var i = 0; i < elements.length; i += 2) {
+      const elements = document.querySelectorAll('input[type="text"]');
+      for (let i = 0; i < elements.length; i += 2) {
+        if (i != 0) newUrl += "&";
         newUrl += elements[i].value + "=" + elements[i + 1].value;
       }
       chrome.tabs.update(undefined, { url: newUrl });
@@ -84,10 +79,18 @@ function addField(key, value){
   value_field.setAttribute("type", "text");
   value_field.setAttribute("value", value);
 
+  let remove = document.createElement("button");
+  remove.setAttribute("id", "delete");
+  remove.setAttribute("class", "remove");
+
+  let trash = document.createElement("i");
+  trash.setAttribute("class", "fa fa-trash-o");
+  remove.appendChild(trash);
+
   span.appendChild(key_field);
   span.appendChild(document.createTextNode(" = "));
   span.appendChild(value_field);
-  span.appendChild(document.createElement("br"));
+  span.appendChild(remove);
 
   div.appendChild(span);
 }
